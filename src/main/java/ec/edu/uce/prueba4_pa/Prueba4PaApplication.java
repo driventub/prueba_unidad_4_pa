42e7a8a6c772f766baac1f79cf36f9f7accfb695
driven <aldazpaulo@gmail.com>
2022-03-30 22:55:39 -0500

    preparando archivos

---
+++ src/main/java/ec/edu/uce/prueba4_pa/Prueba4PaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.prueba4_pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba4PaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba4PaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/uce/prueba4_pa/Prueba4PaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.prueba4_pa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba4PaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
